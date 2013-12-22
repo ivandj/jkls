d396ff6cc68558e7da1eaf992757bb1fd11ff5c7
Ivan <ivandj@gmail.com>
2013-12-21 22:23:07 -0500

    search jq

---
+++ js/tipuesearch_set.js
@@ -0,0 +1,32 @@
+
+/*
+Tipue Search 3.1
+Copyright (c) 2013 Tipue
+Tipue Search is released under the MIT License
+http://www.tipue.com/search
+*/
+
+
+var tipuesearch_stop_words = ["and", "be", "by", "do", "for", "he", "how", "if", "is", "it", "my", "not", "of", "or", "the", "to", "up", "what", "when"];
+
+var tipuesearch_replace = {"words": [
+     {"word": "tipua", replace_with: "tipue"},
+     {"word": "javscript", replace_with: "javascript"}
+]};
+
+var tipuesearch_stem = {"words": [
+     {"word": "e-mail", stem: "email"},
+     {"word": "javascript", stem: "script"},
+     {"word": "javascript", stem: "js"}
+]};
+var tipuesearch_pages = [
+    "http://ivandj.github.io/jkls/",
+    "http://ivandj.github.io/jkls/2013-12-14/hello/",
+    "http://ivandj.github.io/jkls/2013-12-15/welcome-to-jekyll/",
+    "http://ivandj.github.io/jkls/2013-12-21/benefits/",
+    "http://ivandj.github.io/jkls/about/",
+    "http://ivandj.github.io/jkls/contact-info/"
+    ];
+
+
+
